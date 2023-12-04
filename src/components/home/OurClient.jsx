5151b8ad18d43fbcfe50b8ca1b415cc102d5de16
Kemal Zulfiqar <kemalzulfiqar@Kemals-MacBook-M1-Pro.local>
2023-12-04 18:46:21 +0700

    fix our client

--- src/components/home/OurClient.jsx
+++ src/components/home/OurClient.jsx
@@ -16,19 +16,19 @@ export default function OurClient() {
   ];
 
   return (
-    <section className="space-x-6  py-6 md:py-12 md:space-y-12">
+    <section className="py-6 md:py-12 md:space-y-12 overflow-hidden">
       <TittleCustom
         title="OUR CLIENTS"
         subTitle="See who’s our partners"
         className="text-left md:text-center px-6 md:px-[120px] "
       />
-      <Marquee className="flex justify-between " autoFill pauseOnHover>
+      <Marquee className="" autoFill pauseOnHover>
         {clientLogos.map((res, i) => (
           <img
             src={res}
             alt="client"
             key={i}
-            className="mx-6 object-contain h-12 md:h-max max-h-[120px] max-w-[300px] bg-red-300"
+            className="mx-6 object-contain h-12 md:h-max max-h-[120px] max-w-[300px]"
           />
         ))}
       </Marquee>
